36077f56bde0de9c7b71b3f9c8f3f1a0f84542b4
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2017-01-16 21:35:31 +0100

    Team: don't filter slackbot out
    The REST API now automatically filters it out.

--- js/team.js
+++ js/team.js
@@ -21,39 +21,37 @@ angular.module('team', []).controller('teamController', function($scope, $http) 
     $http.get('{{ site.baseurl }}/api/team')
         .success(function(data) {
 
-            //push all non bot members (slackbot has to be eliminated the ugly way)
+            //push all members
             angular.forEach(data.members, function (member) {
-                if (!member.is_bot && member.name != "slackbot"){
-                    //if image is blank, set url to default image
-                    if(member.profile.image_192.indexOf("MISSING") != -1){
-                        member.profile.image_192 = "{{ site.baseurl }}/images/ic_account_circle.png";
-                        member.profile.image_512 = "{{ site.baseurl }}/images/ic_account_circle.png";
-                    }
-
-                    //add twitter, googleplus and github data if it exists
-                    try {
-                        if(membersDataDict[member.real_name]["twitter"] != undefined)
-                            member.twitter = membersDataDict[member.real_name]["twitter"];
-                    }catch(err){}
-                    try {
-                        if(membersDataDict[member.real_name]["googleplus"] != undefined)
-                            member.googleplus = membersDataDict[member.real_name]["googleplus"];
-                    }catch(err){}
-                    try {
-                        if(membersDataDict[member.real_name]["github"] != undefined)
-                            member.github = membersDataDict[member.real_name]["github"];
-                    }catch(err){}
-
-                    //change timezone to its abbreviation
-                    try {
-                        member.tz = moment().tz(member.tz).format('z');
-                    } catch (err) {
-                        console.warn(err);
-                    }
-
-                    //push into members array
-                    team.members.push(member);
+                //if image is blank, set url to default image
+                if(member.profile.image_192.indexOf("MISSING") != -1){
+                    member.profile.image_192 = "{{ site.baseurl }}/images/ic_account_circle.png";
+                    member.profile.image_512 = "{{ site.baseurl }}/images/ic_account_circle.png";
                 }
+
+                //add twitter, googleplus and github data if it exists
+                try {
+                    if(membersDataDict[member.real_name]["twitter"] != undefined)
+                        member.twitter = membersDataDict[member.real_name]["twitter"];
+                }catch(err){}
+                try {
+                    if(membersDataDict[member.real_name]["googleplus"] != undefined)
+                        member.googleplus = membersDataDict[member.real_name]["googleplus"];
+                }catch(err){}
+                try {
+                    if(membersDataDict[member.real_name]["github"] != undefined)
+                        member.github = membersDataDict[member.real_name]["github"];
+                }catch(err){}
+
+                //change timezone to its abbreviation
+                try {
+                    member.tz = moment().tz(member.tz).format('z');
+                } catch (err) {
+                    console.warn(err);
+                }
+
+                //push into members array
+                team.members.push(member);
             });
         })
         .error(function(data) {

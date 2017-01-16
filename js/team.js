873f9c8100215cf16e512dbddf9651b9afcd854d
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2017-01-16 21:28:34 +0100

    Team: fix member data

--- js/team.js
+++ js/team.js
@@ -4,11 +4,12 @@
 //fills all the authors data into one dict (dict because accessing n-th element is
 // faster then iterating over entire array)
 var membersDataDict = {};
-{% for member in site.data.authors %}
-    membersDataDict["{{member.name}}"] = {};
-    {% if member.twitter %} membersDataDict["{{member.name}}"]["twitter"] = "{{member.twitter}}"; {% endif %}
-    {% if member.googleplus %} membersDataDict["{{member.name}}"]["googleplus"] = "{{member.googleplus}}"; {% endif %}
-    {% if member.github %} membersDataDict["{{member.name}}"]["github"] = "{{member.github}}"; {% endif %}
+{% for member_hash in site.data.authors %}
+{% assign member = member_hash[1] %}
+membersDataDict["{{member.name}}"] = {};
+{% if member.twitter %}membersDataDict["{{member.name}}"]["twitter"] = "{{member.twitter}}";{% endif %}
+{% if member.googleplus %}membersDataDict["{{member.name}}"]["googleplus"] = "{{member.googleplus}}";{% endif %}
+{% if member.github %}membersDataDict["{{member.name}}"]["github"] = "{{member.github}}";{% endif %}
 {% endfor %}
 
 //add angular app
